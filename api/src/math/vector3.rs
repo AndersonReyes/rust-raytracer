56d1e55c8aa335528efb7939b9364cf0416aa12e
Anderson Reyes <reyesanderson428@gmail.com>
2021-03-02 12:22:52 -0500

    length squared

--- api/src/math/vector3.rs
+++ api/src/math/vector3.rs
@@ -1,20 +1,25 @@
 use std::ops;
 
 #[derive(PartialEq, Debug, Clone, Copy)]
-pub struct Vector3<T> {
-    pub x: T,
-    pub y: T,
-    pub z: T,
+pub struct Vector3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
-impl<T> Vector3<T> {
+impl Vector3 {
     /// Returns a new vector from x, y, z parameters
-    pub fn new(x: T, y: T, z: T) -> Vector3<T> {
-        Vector3 {x, y, z}
+    pub fn new<T: Into<f64> + Copy>(x: T, y: T, z: T) -> Vector3 {
+        Vector3 {x: x.into(), y: y.into(), z: z.into()}
+    }
+
+    /// Get the squared length of the vector, returns x^2 + y^2 + z^2
+    pub fn length_squared(&self) -> f64 {
+        self.x*self.x + self.y*self.y + self.z*self.z
     }
 }
 
-impl<T: ops::Add<Output = T>> ops::Add for Vector3<T> {
+impl ops::Add for Vector3 {
     type Output = Self;
 
     fn add(self, other: Self) -> Self::Output {
@@ -34,9 +39,9 @@ mod tests {
     #[test]
     fn test_new() {
         let vec = Vector3::new(1, 2, 3);
-        assert_eq!(vec.x, 1);
-        assert_eq!(vec.y, 2);
-        assert_eq!(vec.z, 3);
+        assert_eq!(vec.x, 1.0);
+        assert_eq!(vec.y, 2.0);
+        assert_eq!(vec.z, 3.0);
 
         let vec2 = Vector3::new(1.3, 2.0, 3.5);
         assert_eq!(vec2.x, 1.3);
@@ -51,8 +56,12 @@ mod tests {
 
         let output = vec + two;
         assert_eq!(Vector3::new(5, 7, 9), output);
-        assert_eq!(output.x, 5);
-        assert_eq!(output.y, 7);
-        assert_eq!(output.z, 9);
+        assert_eq!(output.x, 5.0);
+        assert_eq!(output.y, 7.0);
+        assert_eq!(output.z, 9.0);
+    }
+
+    #[test]
+    fn test_squared_length() {
     }
 }
